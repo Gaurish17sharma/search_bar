ab57138357ed76754915d50ff84b93af79d9e8d8
Gaurish Sharma <140238027+Gaurish17sharma@users.noreply.github.com>
2024-03-05 09:24:59 +0530

    Add files via upload

---
+++ search.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Live Search using AJAX</title>
+
+   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
+   <script type="text/javascript" src="script.js"></script>
+
+   <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+    <h1>Live Search</h1>
+    <form>
+        <input type="text" size=40 id="search" placeholder="Search" />
+        <div id="display"></div>
+    </form>
+</body>
+
+</html>
